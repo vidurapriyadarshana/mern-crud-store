8a655f9f5d5f9213a27795d4aaa90a95d34705c9
Vidura Priyadarshana <vidura200427@gmail.com>
2025-05-11 22:14:56 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Server started at http://localhost:5000');
+})
+
+app.get('/', (req , res) => {
+    res.send('Server is ready')
+})
+
+
